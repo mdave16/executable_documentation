86a17743cb440c1369de5525cc3400b4943206a0
Mayur Dave <mdave3927@gmail.com>
2021-11-03 22:52:33 +0000

    :recycle: allow for helper fns to be more modular

--- template.py
+++ template.py
@@ -3,39 +3,39 @@ import sys
 def wait_for_enter():
     input("Press Enter to continue: ")
  
-def create_ssh_keypair(context):
+def create_ssh_keypair(username):
     print("Run:")
-    print("   ssh-keygen -t rsa -f ~/{0}".format(context["username"]))
+    print("   ssh-keygen -t rsa -f ~/{0}".format(username))
     wait_for_enter()
  
-def git_commit(context):
+def git_commit(username):
     print("Copy ~/new_key.pub into the `user_keys` Git repository, then run:")
-    print("    git commit {0}".format(context["username"]))
+    print("    git commit {0}".format(username))
     print("    git push")
     wait_for_enter()
  
-def wait_for_build(context):
+def wait_for_build():
     build_url = "http://example.com/builds/user_keys"
     print("Wait for the build job at {0} to finish".format(build_url))
     wait_for_enter()
  
-def retrieve_user_email(context):
+def retrieve_user_email(username):
     dir_url = "http://example.com/directory"
     print("Go to {0}".format(dir_url))
-    print("Find the email address for user `{0}`".format(context["username"]))
-    context["email"] = input("Paste the email address and press enter: ")
+    print("Find the email address for user `{0}`".format(username))
+    return input("Paste the email address and press enter: ")
  
-def send_private_key(context):
+def send_private_key(email):
     print("Go to 1Password")
     print("Paste the contents of ~/new_key into a new document")
-    print("Share the document with {0}".format(context["email"]))
+    print("Share the document with {0}".format(email))
     wait_for_enter()
  
 if __name__ == "__main__":
-    context = {"username": sys.argv[1]}
-    create_ssh_keypair(context)
-    git_commit(context)
-    wait_for_build(context)
-    retrieve_user_email(context)
-    send_private_key(context)
+    username = sys.argv[1]
+    create_ssh_keypair(username)
+    git_commit(username)
+    wait_for_build()
+    email = retrieve_user_email(username)
+    send_private_key(email)
     print("Done.")
